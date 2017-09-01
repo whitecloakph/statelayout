f0cac2244ba1a8e12f727e1baef9c0d560a146d5
Miguel Fermin <miguel.fermin@whitecloak.com>
2017-09-02 06:58:01 +0800

    Cleanup

--- app/src/main/java/com/whitecloak/statelayout/app/MainActivity.java
+++ app/src/main/java/com/whitecloak/statelayout/app/MainActivity.java
@@ -1,11 +1,7 @@
 package com.whitecloak.statelayout.app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
